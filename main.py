eaef9a3a6350b3422653523a9b3560dec35d8321
Reneil Funa <sc21rf@leeds.ac.uk>
2024-03-18 14:58:46 +0000

    datasets loaded for testing and training

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import torch
 import matplotlib.pyplot as plt
 import numpy as np
+from torch.utils.data import DataLoader
 from dataset import Fingers
 
 def imshow(sample_element):
@@ -11,9 +12,18 @@ def main():
     # Device config
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-train_dir = './data/test/*.png'
-dataset = Fingers(directory=train_dir)
-imshow(dataset[128])
+    # Hyper parameters
+    batch_size = 64
+    n_classes = 6
+
+    # Fingers dataset consists of 21600 128x128 black & white images of left/right hands
+    train_dataset = Fingers('./data/train/*.png')
+    test_dataset = Fingers('./data/test/*.png')
+
+    train_loader = DataLoader(train_dataset, batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size, shuffle=False)
+    
+
 
 if __name__ == "__main__":
     main()
