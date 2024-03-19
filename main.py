c3146a6b164cc266dfd4253a9a765b312ebd9a35
Reneil Funa <sc21rf@leeds.ac.uk>
2024-03-19 20:00:01 +0000

    added list of classes

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import torch
-from torch.utils.data import DataLoader
+import torch.nn as nn
 import torchvision.transforms as transforms
 import numpy as np
 import matplotlib.pyplot as plt
+
+from torch.utils.data import DataLoader
 from dataset import Fingers
+from model import SimpleConv
 
 def imshow(sample_element):
-    plt.imshow(sample_element[0], cmap='gray')
+    plt.imshow(sample_element[0].numpy().reshape((128, 128)), cmap='gray')
     plt.show()
 
 def main():
@@ -14,23 +17,19 @@ def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # Hyper parameters
-    batch_size = 64
+    batch_s = 10
     n_classes = 6
 
     all_transforms = transforms.Compose([transforms.ToTensor()])
 
     # Fingers dataset consists of 21600 128x128 black & white images of left/right hands
-    train_dataset = Fingers('./data/train/*.png')
+    train_dataset = Fingers('./data/train/*.png', transform=all_transforms)
     test_dataset = Fingers('./data/test/*.png', transform=all_transforms)
 
-    train_loader = DataLoader(train_dataset, batch_size, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size, shuffle=False)
-
-    imshow(train_dataset[17])
-    print(test_dataset[17])
-   
+    train_loader = DataLoader(train_dataset, batch_size=batch_s, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_s, shuffle=False)
     
-
+    classes = ('0', '1', '2', '3', '4', '5', '6')
 
 if __name__ == "__main__":
     main()
