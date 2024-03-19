f644cb24d141f561dc914c9988a778a2daa83a89
Reneil Funa <sc21rf@leeds.ac.uk>
2024-03-19 19:59:39 +0000

    built CNN skeleton

--- model.py
+++ model.py
@@ -1,15 +1,9 @@
-import torch
 import torch.nn as nn
 
-class SimpleConv(nn.Module):
-        def __init__(self, input_size, hidden_size, n_classes):
-            super(SimpleConv, self).__init__()
-            self.linear1 = nn.Linear(input_size, hidden_size)
-            self.relu = nn.ReLU()
-            self.linear2 = nn.Linear(hidden_size, n_classes)
+class NeuralNet(nn.Module):
+        def __init__(self):
+            super(NeuralNet, self).__init__()
 
         def forward(self, x):
-            out = self.linear1(x)
-            out = self.relu(out)
-            out = self.linear2(out)
-            return out
+            pass
+            
